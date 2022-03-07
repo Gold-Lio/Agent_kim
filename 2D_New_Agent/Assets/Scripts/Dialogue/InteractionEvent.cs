2909a6c9e65557f8e2767fcb3fbe42587eb93544
joyjoyjoy <81938345+jk-JOY@users.noreply.github.com>
2022-03-07 21:02:25 +0900

    3/7_1

--- 2D_New_Agent/Assets/Scripts/Dialogue/InteractionEvent.cs
+++ 2D_New_Agent/Assets/Scripts/Dialogue/InteractionEvent.cs
@@ -12,4 +12,17 @@ public class InteractionEvent : MonoBehaviour
         dialogue.dialogues = DatabaseManager.instance.GetDialogue((int)dialogue.line.x, (int)dialogue.line.y);           // 자기자신에다가
         return dialogue.dialogues;
     }
+
+    // 대화 정보 출력하는 함수
+    void DebugDialogue(Dialogue[] dialogues)
+    {
+        for (int i = 0; i < dialogues.Length; i++)
+        {
+            // 캐릭터 이름 출력
+            Debug.Log(dialogues[i].name);
+            // 대사들 출력
+            foreach (string context in dialogues[i].contexts)
+                Debug.Log(context);
+        }
+    }
 }
